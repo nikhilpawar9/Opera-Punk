953be032f5d44537419379f1e9d0f9e2ddbd9b43
Nikhil <nikhilpawar982001@gmail.com>
2021-12-22 00:33:56 +0530

    Done with card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard';
 
 function App() {
   return (
     <div className="app">
 
       <Header />
+      <CollectionCard id={0} name={"Bandana Punk"} traits={[{'value':7}]} image='https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu' />
     </div>
   );
 }
